0f67c0242abd5697e893c18564fe87169f05fb7b
Rokas Rozga <rokasrozgadev@gmail.com>
2025-04-07 15:54:57 +0100

    Init

---
+++ backend/src/main/java/dev/rokasrozga/dts_developer_challenge/DtsDeveloperChallengeApplication.java
@@ -0,0 +1,13 @@
+package dev.rokasrozga.dts_developer_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtsDeveloperChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtsDeveloperChallengeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/rokasrozga/dts_developer_challenge/DtsDeveloperChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rokasrozga.dts_developer_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtsDeveloperChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
